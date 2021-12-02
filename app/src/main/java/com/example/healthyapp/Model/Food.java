18bde35794eb63885f64d286c61ec0933d79075f
popescudimi <dimitrie.popescu@yahoo.com>
2021-12-02 18:51:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/healthyapp/Model/Food.java
@@ -0,0 +1,27 @@
+package com.example.healthyapp.Model;
+
+public class Food {
+    private String FoodName;
+    private String FoodDetail;
+    private int FoodCalories;
+    private String FoodLink;
+
+
+    public String getFoodName() {
+        return FoodName;
+    }
+
+    public int getFoodCalories(){
+        return FoodCalories;
+
+    }
+
+    public String getFoodDetail(){
+        return FoodDetail;
+    }
+
+    public String getFoodLink() {
+        return FoodLink;
+    }
+
+}
